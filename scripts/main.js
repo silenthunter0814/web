0fb6616c984882e8285f382bdf82701a2630a5d2
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2023-04-29 20:23:13 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,8 +16,7 @@ function setUserName() {
     const myName = prompt("Please enter your name");
     if (!myName) {
         setUserName();
-    }
-    {
+    } else {
         localStorage.setItem("name", myName);
         myHeading.textContent = "Mozilla is cool " + myName;
     }
