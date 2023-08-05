abca04bfb3e0caee7a54bd34d21c75874b796f0d
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2023-08-05 10:18:24 +0800

    Update ninja.js

--- ninja.js
+++ ninja.js
@@ -1,29 +1,4 @@
-const MouseCounterModule = function() {
-    let numClicks = 0;
-    const handleClick = () => {
-        alert(++numClicks);
-    };
-
-    return {
-        countClicks: () => {
-            document.addEventListener("click", handleClick);
-        }
-    };
-}();
-
-(function(module) {
-    let numScrolls = 0;
-    const handleScroll = () => {
-        alert(++numScrolls);
-    }
-
-    module.countScrolls = () => {
-        document.addEventListener("wheel", handleScroll);
-    };
-})(MouseCounterModule);
-
-
-import { message, sayHiToNinja } from "Ninja.js";
+import { message, sayHiToNinja } from "./Ninja.js";
 
 assert(message === "Hello",
       "We can access the imported variable");
