c9b8a68206092cd48c1182e65fe9cf35dd57ac9f
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2023-08-05 11:42:15 +0800

    Update ninja.js

--- ninja.js
+++ ninja.js
@@ -1,9 +1,10 @@
-import { message, sayHiToNinja } from "./Ninja.js";
+import ImportedNinja from "./Ninja.js";
+import {compareNinjas} from "./Ninja.js";
 
-assert(message === "Hello",
-      "We can access the imported variable");
-assert(sayHiToNinja() === "Hello Yoshi",
-      "We can say hi to Yoshi from outside the module");
-assert(typeof ninja === "undefined",
-      "But we cannot access Yoshi directly");
+const ninja1 = new ImportedNinja("Yoshi");
+const ninja2 = new ImportedNinja("Hattori");
 
+assert(ninja1 !== undefined
+      && ninja2 !== undefined, "We can create a couple of Ninjas");
+assert(!compareNinjas(ninja1, ninja2),
+      "We can compare ninjas");
