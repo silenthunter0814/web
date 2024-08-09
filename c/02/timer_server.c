bcc359397d6e6072abac3212d8e5f5862f02801b
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-09 23:44:02 +0800

    Update timer_server.c

--- c/02/timer_server.c
+++ c/02/timer_server.c
@@ -14,7 +14,7 @@ int main()
 {
     printf("Configuring local address ...\n");
     struct addrinfo hints;
-    memset(&hints, 0, sizeof(htins));
+    memset(&hints, 0, sizeof(hints));
     hints.ai_familly = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
