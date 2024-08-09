051ff4b86a04da277d30684c1f368e20a013dd31
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-09 10:16:12 +0800

    update

--- c/02/timer_server.c
+++ c/02/timer_server.c
@@ -32,7 +32,7 @@ int main()
     }
 
     printf("Binding socket to local address ...\n");
-    if (bind(socket_listen, bind_address->ai_address, bind_address->ai_addrlen)) {
+    if (bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)) {
         fprintf(stderr, "bind() failed, (%d)\n", errno);
         return 1;
     }
@@ -52,5 +52,36 @@ int main()
         fprintf(stderr, "accept() failed (%d)\n", errno);
         return 1;
     }
+
+    printf("Client is connected ...\n");
+    char address_buffer[100];
+    getnameinfo((struct sockaddr *) &client_address, client_len, 
+        address_buffer, sizeof(address_bufffer), 0, 0, NI_NUMERICHOST);
+    printf("%s\n", address_buffer);
+
+    printf("Read request ...\n");
+    char request[1024];
+    int bytes_received = recv(socket_client, request, 1024, 0);
+    printf("Received %d bytes.\n", bytes_received);
+
+    printf("Sending response ...\n");
+    const char *response = 
+        "HTTP/1.1 200 OK\r\n"
+        "Connection: close\r\n"
+        "Content_type: text/plain\r\n\r\n"
+        "Local time is: ";
+    int bytes_sent = send(socket_client, response strlen(response), 0);
+    printf("Sent %d of %d bbytes.\n", bytes_sent, (int)strlen(response));
+
+    time_t timer;
+    time(&timer);
+    char *time_msg = ctime(&timer);
+    bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
+    printf("Send %d of %d bytes.\n", bytes_sent, strlen(time_msg));
+
+    printf("Closing connection ...\n");
+    close(socket_client);
+    close(socket_listen);
+    printf("Finished.\n");
     return 0;
 }
