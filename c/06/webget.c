1574490a971f01f1ca6a3b3449cf1c5191347c87
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-13 15:28:54 +0800

    Update webget.c

--- c/06/webget.c
+++ c/06/webget.c
@@ -7,4 +7,49 @@ int main()
 
 void parse_url(char *url, char **hostname, char **port, char **path) {
     printf("URL: %s\n", url);
+
+    char *p;
+    p = strstr(url, "://");
+
+    char *protocol = 0;
+    if (p) {
+        protocol = url;
+        *p = 0;
+        p += 3;
+    } else {
+        p = url;
+    }
+
+    if (protocol) {
+        if (strcmp(protocol, "http")) {
+            fprintf(stderr, "Unknown protocol '%s'. Only 'http' is supported.\n");
+            exit(1);
+        }
+    }
+
+    *hostname = p;
+    while (*p && *p != ':' && *p != '/' && *p != '#')
+        ++p;
+
+    *port = "80";
+    if (*p = ':') {
+        *p++ = 0;
+        *port = p;
+    }
+    while (*p && *p != '/' && *p != '#')
+        ++p;
+
+    *path = p;
+    if (*p == '/')
+        *path = p + 1;
+    *p = 0;
+
+    while (*p && *p != '#')
+        ++p;
+    if (*p == '#')
+        *p = 0;
+
+    printf("hostname: %s\n", *hostname);
+    printf("port: %s\n", *port);
+    printf("path: %s\n", *path);
 }
