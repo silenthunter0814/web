8ac06f39b42b11bdf4d51a310e4548cec901d8ed
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-13 19:16:04 +0800

    Update webget.c

--- c/06/webget.c
+++ c/06/webget.c
@@ -1,4 +1,15 @@
-#include "chap06.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <clock.h>
 
 void parse_url(char *url, char **hostname, char **port, char **path);
 
