db068d4b0ed4f52bb3213d6f9f7e66c644ace409
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-14 19:22:36 +0800

    Update smtp_send.c

--- c/08/smtp_send.c
+++ c/08/smtp_send.c
@@ -23,4 +23,10 @@ int main(int argc, char *argv[])
 void get_input(const char *prompt, char *buffer)
 {
     printf("%s", prompt);
+
+    buffer[0] = 0;
+    fgets(buffer, MAXINPUT, stdin);
+    const int read = strlen(buffer);
+    if (read > 0)
+        buffer[read - 1] = 0;
 }
