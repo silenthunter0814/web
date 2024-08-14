07e910858532a52be6c47d8db3c1a36458143a7a
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-14 19:52:36 +0800

    Update smtp_send.c

--- c/08/smtp_send.c
+++ c/08/smtp_send.c
@@ -20,6 +20,23 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+int parse_response(const char *response) {
+    const char *k = response;
+    if (!k[0] || !k[1] || !k[2]) return 0;
+    for (; k[3]; ++k) {
+        if (k == response || k[-1] == '\n') {
+            if (isdigit(k[0]) && isdigit(k[1]) && isdigit(k[2])) {
+                if (k[3] != '-' {
+                    if (strstr(k, "\r\n")) {
+                        return strtol(k, 0, 10);
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
+
 void send_format(int server, const char *text, ...) {
     char buffer[1024];
     va_list args;
