e8c6864e633bbcba9da4988bc9b1488cabd6b499
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-14 21:26:46 +0800

    Update smtp_send.c

--- c/08/smtp_send.c
+++ c/08/smtp_send.c
@@ -17,6 +17,69 @@
 
 int main(int argc, char *argv[])
 {
+    char hostname[MAXINPUT];
+    get_input("mail server: ", hostname);
+
+    printf("Connecting to host: %s:25\n", hostname);
+
+    int server = connect_to_host(hostname, "25");
+
+    wait_on_response(server, 220);
+
+    send_format(server, "HELO HONPWC\r\n");
+    wait_on_response(server, 250);
+
+    char sender[MAXINPUT];
+    get_input("from: ", sender);
+    send_format(server, "MAIL FROM:<%s>\r\n", sender);
+    wait_on_response(server, 250);
+
+    char recipient[MAXINPUT];
+    get_input("to: ", recipient);
+    send_format(server, "RCPT TO:<%s>\r\n", recipient);
+
+    send_format(server, "DATA\r\n");
+    wait_on_response(server, 354);
+
+    char subject[MAXINPUT];
+    get_input("subject: ", subject);
+
+    send_format(server, "From:<%s>\r\n", sender);
+    send_format(server, "To:<%s>\r\n", recipient);
+    send_format(server, "Subject:%s\r\n", subject);
+
+    time_t timer;
+    time(&timer);
+
+    struct tm *timeinfo;
+    timeinfo = gmtime(&timer);
+
+    char date[128];
+    strftime(date, "%a, %d %b %Y %H:%M:%S +0000", timeinfo);
+
+    send_format(server, "Date:%s\r\n", date);
+
+    send_format(server, "\r\n");
+
+    printf("Enter your email text, end with \".\" on a line by itself.\n");
+    while (1) {
+        char body[MAXINPUT];
+        get_input("> ", body);
+        send_format(server, "%s\r\n", body);
+        if (strcmp(body, ".") == 0) {
+            break;
+        }
+    }
+
+    wait_on_response(server, 250);
+
+    send_format(server, "QUIT\r\n");
+    wait_on_response(server, 221);
+
+    printf("\nCloseing socket...\n");
+    close(server);
+
+    printf("Finished.\n");
     return 0;
 }
 
