f0266d631a060d120fc890ccdde70fe9b95374a4
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-14 08:26:00 +0800

    Update web_server.c

--- c/07/web_server.c
+++ c/07/web_server.c
@@ -11,11 +11,49 @@
 #include <string.h>
 
 const char *get_content_type(const char* path);
+int create_socket(const char* host, const char *port);
 
 int main(int argc, char *argv[])
 {
 
     return 0;
+}
+
+int create_socket(const char* host, const char *port) {
+    printf("Configuring local address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    
+    struct addrinfo *bind_address;
+    getaddrinfo(host, port, &hints, &bind_address);
+
+    printf("Creating socket...\n");
+    int socket_listen;
+    socket_listen = socket(bind_address->aifamily,
+            bind_address->ai_socktype, bind_address->ai_protocol);
+    if (socket_listen < 0) {
+        fprintf(stderr, "socket() failed. (%d)\n", errno);
+        exit(1);
+    }
+
+    printf("Binding socket to local address...\n");
+    if (bind(socket_listen,
+            bind_address->ai_addr, bind_address->ai_addrlen)) {
+        fprintf(stderr, "bind() failed. (%d)\n", errno);
+        exit(1);
+    }
+    freeaddrinfo(bind_address);
+
+    printf("Listening...\n");
+    if (listen(socket_listen, 10) < 0) {
+        fprintf(stderr, "listen() failed. (%d)\n", errno);
+        exit(1);
+    }
+
+    return socket_listen;
 }
 
 const char *get_content_type(const char* path) {
