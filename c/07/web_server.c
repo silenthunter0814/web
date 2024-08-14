01959ce1ce5e30c22de0503973122bc14b8bcf03
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-14 14:58:17 +0800

    Update web_server.c

--- c/07/web_server.c
+++ c/07/web_server.c
@@ -40,6 +40,7 @@ int main(int argc, char *argv[])
     while (1) {
         fd_set reads;
         reads = wait_on_clients(server);
+        
         if (FD_ISSET(server, &reads)) {
             struct client_info *client = get_client(-1);
             client->socket = accept(server,
@@ -54,6 +55,52 @@ int main(int argc, char *argv[])
                     get_client_address(client));
         }
 
+        struct client_info *client = clients;
+        while (client) {
+            struct client_info *next = client->next;
+
+            if (FD_ISSET(client->socket, &reads)) {
+                if (MAX_REQUEST_SIZE == client->received) {
+                    send_400(client);
+                    continue;
+                }
+
+                int r = recv(client->socket,
+                        client->request + client->received,
+                        MAX_REQUEST_SIZE - client->received, 0);
+                if (r < 1) {
+                    printf("Unexpected disconnect from %s.\n",
+                                get_client_address(client);
+                    drop_client(client);
+                } else {
+                    client->received += r;
+                    client->request[client->received] = 0;
+
+                    char *q = strstr(client->request, "\r\n\r\n");
+                    if (q) {
+                        if (strncmp("GET /", client->request, 5)) {
+                            send_400(client);
+                        } else {
+                            char *path = client->request + 4;
+                            char *end_path = strstr(path, " ");
+                            if (!end_path) {
+                                send_400(client);
+                            } else {
+                                *end_path = 0;
+                                serve_resource(client, path);
+                            }
+                        }
+                    } // if (q)
+                }
+            }
+            client = next;
+        }
+    } // while (1)
+
+    printf("\nClosing socket...\n");
+    close(server);
+
+    printf("Finished.\n");
     return 0;
 }
 
