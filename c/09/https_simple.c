fadbd1e269cc7c8b9b13bc46bee9c61a854d68cc
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-15 16:12:30 +0800

    Create https_simple.c

---
+++ c/09/https_simple.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
