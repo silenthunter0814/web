2f1bfeeea6006d78bb6e8641f89b73afc8611d20
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-15 08:47:36 +0800

    Update openssl.c

--- c/09/openssl.c
+++ c/09/openssl.c
@@ -2,3 +2,9 @@
 SSL_libray_init();
 OpenSSL_add_all_algorithms();
 SSL_load_error_string();
+
+SSL_CTL *ctx = SSL_CTX_new(TLS_client_method());
+if (!ctx) {
+    fprintf(stderr, "SSL_CTX_new() failed.\n");
+    return 1;
+}
