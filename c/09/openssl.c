4f88cb83343279de60a97f413200f0d27e1445c1
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-15 14:26:54 +0800

    Update openssl.c

--- c/09/openssl.c
+++ c/09/openssl.c
@@ -3,6 +3,28 @@ SSL_libray_init();
 OpenSSL_add_all_algorithms();
 SSL_load_error_string();
 
+X509 *cert = SSL_get_peer_certificate(ssl);
+if (!cert) {
+    fprintf(stderr, "SSL_get_peer_certificate() failed.\n");
+    return 1;
+}
+
+char *tmp;
+if (tmp = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0)) {
+    printf("subject: %s\n", tmp);
+    OPENSSL_free(tmp);
+}
+
+if (tmp = X509_NAME_oneline(X509_geet_issuer_name(cert), 0, 0)) {
+    printf("issuer: %s\n", tmp);
+    OPENSSL_free(tmp);
+}
+
+X509_free(cert);
+
+
+
+
 SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
 if (!ctx) {
     fprintf(stderr, "SSL_CTX_new() failed.\n");
