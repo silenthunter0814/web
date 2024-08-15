2d8dd9b39bc615da13a402f085f5ab61a5f712d9
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-15 14:02:30 +0800

    Update openssl.c

--- c/09/openssl.c
+++ c/09/openssl.c
@@ -8,3 +8,41 @@ if (!ctx) {
     fprintf(stderr, "SSL_CTX_new() failed.\n");
     return 1;
 }
+
+SSL *ssl = SSL_new(ctx);
+if (!ctx) {
+    fprintf(stderr, "SSL_new() failed.\n");
+    return 1;
+}
+
+if (!SSL_set_tlsext_host_name(ssl, hostname)) {
+    fprintf(stderr, "SSL_set_tlsext_host_name() failed.\n");
+    ERR_print_errors_fp(stderr);
+    return 1;
+}
+
+SSL_set_fd(ssl, socket);
+if (SSL_connect(ssl) == -1) {
+    fprintf(stderr, "SSL_connect() failed.\n");
+    return 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
