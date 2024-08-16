393dd2faf281b49e557cfc1982a7a5a1f9e29ece
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-16 09:31:23 +0800

    Update tls_time_server.c

--- c/10/tls_time_server.c
+++ c/10/tls_time_server.c
@@ -68,6 +68,75 @@ int main()
         fprintf(stderr, "listen() failed. (%d)\n", errno);
         return 1;
     }
-    
+
+    while (1) {
+        printf("Waiting for connection...\n");
+        struct sockaddr_storage client_address;
+        socklen_t client_len = sizeof(client_address);
+        int socket_client = accept(socket_listen,
+                (struct sockaddr *) &client_address, &client_len);
+        if (socket_client < 0) {
+            fprintf(stderr, "accept() failed. (%d)\n", errno);
+            return 1;
+        }
+
+        printf("Client is connected... ");
+        char address_buffer[100];
+        getnameinfo((struct sockaddr *) &client_address, client_len,
+                address_buffer, sizeof(address_buffer), 0, 0,
+                NI_NUMERICHOST);
+        printf("%s\n", address_buffer);
+
+        SSL *ssl = SSL_new(ctx);
+        if (!ctx) {
+            fprintf(stderr, "SSL_new() failed.\n");
+            return 1;
+        }
+
+        SSL_set_fd(ssl, socket_client);
+        if (SSL_accept(ssl) <= 0) {
+            fprintf(stderr, "SSL_accept() failed.\n");
+            ERR_print_errors_fp(stderr);
+
+            SSL_shutdown(ssl);
+            close(socket_client);
+            SSL_free(ssl);
+
+            continue;
+        }
+
+        printf("SSL connection using %s\n", SSL_get_cipher(ssl));
+
+        printf("Reading request...\n");
+        char request[1024];
+        int bytes_received = SSL_read(ssl, request, 1024);
+        printf("Received %d bytes.\n", bytes_received);
+
+        printf("Sending response...\n");
+        const char *response =
+            "HTTP/1.1 200 OK\r\n"
+            "Connection: close\r\n"
+            "Content-Type: text/plain\r\n\r\n"
+            "Local time is: ";
+        int bytes_sent = SSL_write(ssl, response, strlen(response));
+        printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
+
+        time_t timer;
+        time(&timer);
+        char *time_msg = ctime(&timer);
+        bytes_sent = SSL_write(ssl, time_msg, strlen(time_msg));
+        printf("Send %d of %d bytes.\n", bytes_sent, (int)strlen(time_msg));
+
+        printf("Closing connection...\n");
+        SSL_shutdown(ssl);
+        close(socket_client);
+        SSL_free(ssl);
+    }
+
+    printf("Closing listening socket...\n");
+    close(socket_listen);
+    SSL_CTX_free(ctx);
+
+    printf("Finished.\n");
     return 0;
 }
