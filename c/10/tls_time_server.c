7e8bc6e9a9105dc451d507ac1025ff16914d4fa1
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-08-16 08:44:55 +0800

    Update tls_time_server.c

--- c/10/tls_time_server.c
+++ c/10/tls_time_server.c
@@ -31,6 +31,29 @@ int main()
 
     if (!SSL_CTX_use_certificate_file(ctx, "cert.pem" , SSL_FILETYPE_PEM)
         || !SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM)) {
+        fprintf(stderr, "SSL_CTX_use_certificate_file() failed.\n");
+        ERR_print_errors_fp(stderr);
+        return 1;
     }
+
+    priintf("Configuring local address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints_ai_flags = AI_PASSIVE;
+
+    struct addrinfo *bind_address;
+    getaddrinfo(0, "8080", &hints, &bind_address);
+
+    printf("Creating socket...\n");
+    int socket_listen;
+    socket_listen = socket(bind_address->ai_family,
+            bind_address->ai_socktype, bind_address->ai_protocol);
+    if (socket_listen < 0) {
+        fprintf(stderr, "socket() failed. (%d)\n", errno);
+        return 1;
+    }
+    
     return 0;
 }
